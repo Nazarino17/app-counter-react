1f9936a58eb1661c5250e221c1af3aac447262c1
Nazar <nazar_pilipenko@inbox.ru>
2023-08-30 18:19:04 +0300

    counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: this.props.counter
+    }
+  }
+  
+  incPlus = () => {
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+  }
+  decMinys = () => {
+    this.setState(state => ({
+      counter: state.counter - 1
+    }));
+  }
+
+  rndNum = () => {
+    this.setState(({
+        counter: +(Math.random() * (50 - -50) + -50).toFixed(0)
+    }));
+  }
+
+  resetN = () => {
+    this.setState(({
+      counter: this.props.counter
+    }));
+  }
+
+  render() {
+    const {counter} = this.state;
+    return (
+      <div className="app">
+        <div className="counter">{counter}</div>
+        <div className="controls">
+          <button onClick={this.incPlus}>INC</button>
+          <button onClick={this.decMinys}>DEC</button>
+          <button onClick={this.rndNum}>RND</button>
+          <button onClick={this.resetN}>RESET</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
